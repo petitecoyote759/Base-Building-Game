ced97a78e2c48b5e5b01276fabce97c5eda619c0
Matthew Short <matthew.short.nh@gmail.com>
2024-06-26 13:48:29 +0100

    Adding base files

---
+++ Handler/Handler.cs
@@ -0,0 +1,16 @@
+﻿using Short_Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Building_Game
+{
+    public static partial class General
+    {
+        public class Handler : ShortHandler
+        {
+        }
+    }
+}
