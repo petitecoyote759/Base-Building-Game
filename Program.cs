871c3d258c39cc58a4785a73c9472dd4e9ea0fd9
Matthew Short <matthew.short.nh@gmail.com>
2024-06-26 13:34:50 +0100

    Testing these

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿
+﻿using Short_Tools;
+using static Short_Tools.General;
+using Debugger = Short_Tools.ShortDebugger;
+using SDL2;
+using static SDL2.SDL;
 
-
-public static partial class General
+namespace Base_Building_Game
 {
-
+    public static partial class General
+    {
+        public static void Main()
+        {
+            Print("Test"); 
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Base Building Game
+
+The game
