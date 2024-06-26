a123b9c6a5c440b9ac5ee8f7e102e2d83229e5f6
Matthew Short <matthew.short.nh@gmail.com>
2024-06-26 17:27:55 +0100

    Added the handler

--- Handler/Handler.cs
+++ Handler/Handler.cs
@@ -9,9 +9,38 @@ namespace Base_Building_Game
 {
     public static partial class General
     {
+        static Dictionary<string, bool> ActiveKeys = new Dictionary<string, bool>()
+        {
+            { "w", false },
+            { "a", false },
+            { "s", false },
+            { "d", false },
+        }; // the keys currently being pressed
+
+
+
+
+
         public class Handler : ShortHandler
         {
-            public Handler() : base() { }
+
+            public Handler() : base() { } // stick in base(Flag.Debug) to see what buttons are pressed 
+
+            public override void Handle(string inp, bool down)
+            {
+                if (ActiveKeys.ContainsKey(inp)) { ActiveKeys[inp] = down; }
+
+
+
+                switch (inp)
+                {
+                    case "SPACE":
+
+                        if (!InGame) { InGame = true; }
+
+                        break;
+                }
+            }
         }
     }
 }
