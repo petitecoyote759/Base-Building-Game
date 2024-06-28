d2d1ff79ccadf6bc26a5d2571b8b84bbf627e2b8
petitecoyote759 <matthew.short.nh@gmail.com>
2024-06-28 13:37:00 +0100

    Added a "Make2DArray" function, was useful to me

--- Main/Functions.cs
+++ Main/Functions.cs
@@ -128,5 +128,20 @@ namespace Base_Building_Game
 
 
 
+
+
+
+        private static T[,] Make2DArray<T>(T[] input, int height, int width)
+        {
+            T[,] output = new T[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    output[i, j] = input[i * width + j];
+                }
+            }
+            return output;
+        }
     }
 }
