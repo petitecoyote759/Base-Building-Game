2ed842a1b8afd21df9e19c91d7619b6979da272e
TheScroobly <sikandarakhan2007@gmail.com>
2024-06-30 13:59:56 +0100

    deloads

---
+++ World/WorldDeloader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Building_Game
+{
+    public static partial class General
+    {
+        public static void WorldDeloader()
+        {
+            for (int i = 0; i < World.size; i++) 
+            {
+                for (int j = 0; j < World.size; j++) 
+                {
+                    world.sectors[i, j].map = null;
+                }
+            }
+
+
+        }
+
+    }
+}
