0218be89b592c3b24d37e4cc37450071306cc475
petitecoyote759 <matthew.short.nh@gmail.com>
2024-07-20 20:04:24 +0100

    Added conveyor file

---
+++ World/Buildings/Individuals/Conveyor.cs
@@ -0,0 +1,19 @@
+﻿using Short_Tools;
+using System.Numerics;
+using static Short_Tools.General;
+using static System.MathF;
+using static System.Numerics.Vector2;
+using IVect = Short_Tools.General.ShortIntVector2;
+using Priority = Short_Tools.ShortDebugger.Priority;
+using V2 = System.Numerics.Vector2;
+
+
+
+
+namespace Base_Building_Game
+{
+    public static partial class General
+    {
+
+    }
+}
