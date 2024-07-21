4abcb9c0744fb50519e60b639c05cf79827951bb
petitecoyote759 <matthew.short.nh@gmail.com>
2024-07-21 15:33:10 +0100

    Added interface for having health

---
+++ Entities/Misc interfaces.cs
@@ -0,0 +1,23 @@
+﻿using Short_Tools;
+using System.Numerics;
+using static Short_Tools.General;
+using static System.MathF;
+using static System.Numerics.Vector2;
+using IVect = Short_Tools.General.ShortIntVector2;
+using Priority = Short_Tools.ShortDebugger.Priority;
+using V2 = System.Numerics.Vector2;
+
+
+
+
+namespace Base_Building_Game.Entities
+{
+    public static partial class General
+    {
+        public interface IHasHealth
+        {
+            public int MaxHealth { get; }
+            public int CurrentHealth { get; set; }
+        }
+    }
+}
