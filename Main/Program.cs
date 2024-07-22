6535377b0fbdbae8fa4eb27112d1e4dcb2e9a3b0
petitecoyote759 <matthew.short.nh@gmail.com>
2024-07-22 19:24:40 +0100

    N/A

--- Main/Program.cs
+++ Main/Program.cs
@@ -34,6 +34,7 @@ namespace Base_Building_Game
             LoadText();
             LoadCutscenes();
 
+
             SetSeed(); // <- stick a seed in here to generate the same world :) (you can get the seed via the RandSeed var)
 
 
