1eb3a1814452221a06da78918fbc9f8a03106148
mattie <matthew.short.nh@gmail.com>
2024-08-03 22:27:36 +0100

    Added menu drawer, and added drawing for the start menu

---
+++ Renderer/Menus/MenuDrawer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDL2;
+using static SDL2.SDL;
+using Short_Tools;
+using static Short_Tools.General;
+using System.Runtime.CompilerServices;
+
+
+
+
+
+
+namespace Base_Building_Game
+{
+    public static partial class General
+    {
+        public partial class Renderer
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void DrawMenu()
+            {
+                switch (MenuState)
+                {
+                    case MenuStates.StartScreen:
+
+                        Draw(0, 0, screenwidth, screenheight, "Background");
+
+                        byte rgb = 0;
+
+                        unchecked
+                        {
+                            rgb = (byte)(255 * (Math.Sin(DateTimeOffset.Now.ToUnixTimeMilliseconds() / 500d) + 2) / 4);
+                        }
+
+                        Write(
+                            (screenwidth - (45 * 20)) / 2, // length of "Press SP...
+                            screenheight * 6 / 10, 
+                            60, 
+                            60, 
+                            "Press SPACE to start", 
+                            new SDL_Color() { a = 255, r = rgb, b = rgb, g = rgb });
+
+                        break;
+                }
+            }
+        }
+    }
+}
