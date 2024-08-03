2f86d3eaf2e75a59e4619f22ce494940d9e68ca4
mattie <matthew.short.nh@gmail.com>
2024-08-03 21:55:23 +0100

    Added menu enum to show state

---
+++ Renderer/Menus/Menus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Short_Tools.General;
+using Short_Tools;
+
+
+
+
+
+
+
+namespace Base_Building_Game
+{
+    public enum MenuStates
+    {
+        StartScreen,
+
+        SelectOnlineOffline,
+
+        OfflineCreateLoadWorld,
+
+
+        /// <summary>
+        /// Offline menu for creating a world, should have an option to put in a seed, world size, and uhhh idk
+        /// difficulty too? and cheats + structures, make it so it is extendable with click to change
+        /// </summary>
+        OfflineCreateWorld,
+
+
+        /// <summary>
+        /// Offline menu for loading a world, should pop up with all of the worlds in your folder, scroll bar.
+        /// when you press on a world, show some stats about it (playtime, version, time last played)
+        /// </summary>
+        OfflineLoadWorld,
+
+
+
+
+        InGame
+    }
+}
