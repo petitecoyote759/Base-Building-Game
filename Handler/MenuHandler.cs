f1553f4a62a472b08cbfc2e8d12c1786b68d1144
mattie <matthew.short.nh@gmail.com>
2024-08-03 23:49:16 +0100

    Added a menu handler

---
+++ Handler/MenuHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Building_Game
+{
+    public static partial class General
+    {
+        public static void HandleMenus(string inp)
+        {
+            switch (MenuState)
+            {
+                case MenuStates.StartScreen:
+
+                    if (inp == "SPACE")
+                    {
+                        MenuState = MenuStates.SelectOnlineOffline;
+                    }
+
+                    break;
+
+
+                case MenuStates.SelectOnlineOffline:
+
+                    if (inp == "ESCAPE")
+                    {
+                        MenuState = MenuStates.StartScreen;
+                    }
+
+                    break;
+            }
+        }
+    }
+}
