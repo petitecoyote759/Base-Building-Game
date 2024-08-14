e67c9c1522a501a20f0c3ae7984e1c11ba1637c4
TheScroobly <sikandarakhan2007@gmail.com>
2024-08-14 04:05:57 +0100

    create fluid folder and fluid container, container has nothing rn

--- Fluids/Fluid.cs
+++ Fluids/Fluid.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Short_Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using static Short_Tools.General;
+using IVect = Short_Tools.General.ShortIntVector2;
 namespace Base_Building_Game
 {
     public partial class General 
@@ -14,12 +18,7 @@ namespace Base_Building_Game
             {
                 Water = 1,
                 Oil
-
             }
-
-
-
-            
         }
     }
     
---
+++ Fluids/FluidContainer.cs
@@ -0,0 +1,24 @@
+﻿using Short_Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Short_Tools.General;
+using IVect = Short_Tools.General.ShortIntVector2;
+
+namespace Base_Building_Game
+{
+    public partial class General
+    {
+        public interface FluidContainer
+        {
+
+
+
+
+        }
+    }
+}
