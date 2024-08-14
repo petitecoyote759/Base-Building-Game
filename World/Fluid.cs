543eeb762e005dac46d8181269b0aa323a9904a5
TheScroobly <sikandarakhan2007@gmail.com>
2024-08-14 01:56:00 +0100

    dont know what happening!!!

---
+++ World/Fluid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Building_Game
+{
+    public partial class General 
+    {
+        public static class Fluid
+        {
+            public enum FluidID : short 
+            {
+                Water = 1,
+                Oil
+
+            }
+
+
+
+            
+        }
+    }
+    
+}
